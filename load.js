00fc568845bc54af34acc9aa12ff0d1b43c50271
shane.booth <shaney.booth@gmail.com>
2021-02-25 18:24:45 +0000

    Removed debug code

--- load.js
+++ load.js
@@ -1,6 +1,5 @@
 const BundleURL = `${document.currentScript.src.replace("/load.js", "")}/dist/bundle.js`
 
-CONFIG.debug.hooks = true
 Hooks.on("reactReady", (loader)=>{
     loader("MorrowinDnD", BundleURL)
 })
