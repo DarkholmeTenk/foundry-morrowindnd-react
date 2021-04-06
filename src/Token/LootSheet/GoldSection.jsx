452d7749475cf7374e4054d782f1e9d9c8b6f754
shane.booth <shaney.booth@gmail.com>
2021-04-06 18:06:58 +0100

    Added in a way to change the amount of gold in a loot sheet

--- src/Token/LootSheet/GoldSection.jsx
+++ src/Token/LootSheet/GoldSection.jsx
@@ -1,9 +1,10 @@
 import GoldDisplay from "../../Util/Components/GoldDisplay";
-import {Button, Card, CardActions, CardContent, CardHeader, Checkbox} from "@material-ui/core";
-import {useCallback} from "react";
+import {Button, Card, CardActions, CardContent, CardHeader, Checkbox, Input} from "@material-ui/core";
+import {useCallback, useState} from "react";
 import {getGoldDetails, useLootSheetFlag} from "./LootSheetGoldUtil";
 import {LootSplitGold} from "./LootAction";
 import {getActorId} from "../../Util/Identifiers/ActorID";
+import {addGold, removeGold} from "../../Util/Helper/GoldHelper";
 
 
 function GoldTakerUser({user, takers, setTakers, disabled}) {
@@ -18,12 +19,22 @@ export default function GoldSection({npc, disabled}) {
     let {flag, setFlag} = useLootSheetFlag(npc)
 
     let {users, takers, amount, splitAmount, takeCount} = getGoldDetails(npc)
+    let [newGold, setNewGold] = useState("0")
     let setTakers=useCallback((newTakers)=> {
         setFlag({...flag, goldTakers: newTakers})
     }, [takers, setFlag])
     let loot = useCallback(()=>{
         LootSplitGold({lootId: getActorId(npc)})
     }, [npc])
+    let setGold = useCallback(async ()=>{
+        let ng = parseFloat(newGold)
+        if(ng > 0) {
+            await addGold(npc, ng)
+        } else if(ng < 0) {
+            await removeGold(npc, -ng)
+        }
+    }, [newGold])
+    let owner = npc.owner
 
     return <Card>
         <CardHeader title="Gold" />
@@ -37,9 +48,13 @@ export default function GoldSection({npc, disabled}) {
                 Split:
                 <GoldDisplay value={splitAmount} />
             </div>
+            {owner ? <div>
+                <Input title="Add Gold" value={newGold} onChange={(e)=>setNewGold(e.target.value)} />
+            </div> : null}
         </CardContent>
         <CardActions>
             <Button disabled={takeCount === 0 || amount === 0} onClick={loot}>Loot Gold</Button>
+            {owner ? <Button onClick={setGold}>Add Gold</Button> : null}
         </CardActions>
     </Card>
 }
